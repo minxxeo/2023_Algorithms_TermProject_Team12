48c21671137bbf03bc289916b7a089dcd5f4f466
waterlemon7z <lemon7z@icloud.com>
2023-12-02 02:28:11 +0900

    [ADD] A* algorithm

--- out/production/2023-Algorithms/src/algorithms/Dfs.java
+++ out/production/2023-Algorithms/src/algorithms/Dfs.java
@@ -5,6 +5,7 @@ import java.util.*;
 import entity.ConzonNode;
 import javafx.util.Pair;
 import util.ConzonInfo;
+import util.ConzonNameLoc;
 
 public class Dfs
 {
@@ -12,7 +13,7 @@ public class Dfs
     int[] prev_loc = new int[1100];
     int[] time = new int[1100];
     private final List<List<ConzonNode>> adjacent;
-    private final Map<Integer, String> conzonDict;
+    private final Map<Integer, ConzonNameLoc> conzonDict;
     private final Map<Integer, String> lineInfo;
 
     public Dfs()
@@ -28,23 +29,6 @@ public class Dfs
         dijkstra(from, mode);
         int cur = to;
         List<Integer> line = new ArrayList<>();
-        while (true)
-        {
-            line.add(cur);
-            if (cur == from)
-                break;
-            cur = prev_loc[cur];
-        }
-        return new Pair<>(dist[to], line);
-    }
-
-    //    public Pair<Integer, List<Integer>> find_A_to_B(int from, int to )
-//    {
-//        /*Test graph connectivity*/
-//        dijkstra(from, 1);
-//
-//        int cur = to;
-//        List<Integer> line = new ArrayList<>();
 //        while (true)
 //        {
 //            line.add(cur);
@@ -52,8 +36,23 @@ public class Dfs
 //                break;
 //            cur = prev_loc[cur];
 //        }
-//        return new Pair<>(dist[to],line);
-//    }
+        return new Pair<>(dist[to], line);
+    }
+    public Pair<Integer, List<Integer>> getStart2EndWithAStar(int from, int to, int mode)
+    {
+        aStar(from, to);
+        int cur = to;
+        List<Integer> line = new ArrayList<>();
+//        while (true)
+//        {
+//            line.add(cur);
+//            if (cur == from)
+//                break;
+//            cur = prev_loc[cur];
+//        }
+        return new Pair<>(dist[to], line);
+    }
+
     void dijkstra(int from, int mode) //By distance mode 1 : dist, mode 2 : time
     {
         Arrays.fill(dist, 2147483647);
@@ -61,6 +60,8 @@ public class Dfs
         PriorityQueue<Node> pq = new PriorityQueue<>();
         int newdist;
         dist[from] = 0;
+        boolean[] fin = new boolean[1100];
+        fin[from] = true;
         pq.offer(new Node(from, 0));
         while (!pq.isEmpty())
         {
@@ -68,44 +69,54 @@ public class Dfs
             for (ConzonNode curNode : adjacent.get(cur))
             {
                 newdist = dist[cur] + (mode == 1 ? curNode.getDist() : curNode.getTime());
-                if (dist[curNode.getId()] > newdist)
+                if (dist[curNode.getId()] > newdist&& !fin[curNode.getId()])
                 {
                     dist[curNode.getId()] = newdist;
                     prev_loc[curNode.getId()] = cur;
+                    fin[curNode.getId()] = true;
                     pq.offer(new Node(curNode.getId(), newdist));
                 }
             }
         }
     }
-//    void dijkstra_speed(int from) //By distance
-//    {
-//        Arrays.fill(time, 2147483647);
-//        Arrays.fill(prev_loc, 0);
-//        PriorityQueue<Node> pq = new PriorityQueue<>();
-//        time[from] = 0;
-//        pq.offer(new Node(from, 0));
-//        while(!pq.isEmpty())
-//        {
-//            int cur = pq.poll().idx;
-//            for(ConzonNode curNode : adjacent.get(cur))
-//            {
-//                if(time[curNode.getId()] > time[cur] + curNode.getTime())
-//                {
-//                    time[curNode.getId()] = time[cur] + curNode.getTime();
-//                    prev_loc[curNode.getId()] = cur;
-//                    pq.offer(new Node(curNode.getId(), time[cur] + curNode.getTime()));
-//                }
-//            }
-//        }
-//    }
+
+    void aStar(int from, int to)
+    {
+        Arrays.fill(dist, 2147483647);
+        Arrays.fill(prev_loc, 0);
+        PriorityQueue<AStarNode> pq = new PriorityQueue<>();
+        int[][] linedist = ConzonInfo.getLinedist();
+        int newdist;
+        dist[from] = 0;
+        boolean[] fin = new boolean[1100];
+        pq.offer(new AStarNode(from, 0, linedist[from][to]));
+        while (!pq.isEmpty())
+        {
+            int cur = pq.poll().idx;
+            if (cur == to) {
+                break;
+            }
+            for (ConzonNode curNode : adjacent.get(cur))
+            {
+                newdist = dist[cur] + curNode.getDist();
+                if (dist[curNode.getId()] > newdist && !fin[curNode.getId()])
+                {
+                    dist[curNode.getId()] = newdist;
+                    prev_loc[curNode.getId()] = cur;
+                    fin[curNode.getId()] = true;
+                    pq.offer(new AStarNode(curNode.getId(), newdist, linedist[curNode.getId()][to]));
+                }
+            }
+        }
+    }
 
     public void printRoute(List<Integer> line)
     {
         for (int i = line.size() - 1; i >= 0; i--)
         {
-            if(conzonDict.get(line.get(i)).contains("IC") && !(i == 0 || i == line.size() - 1) )
+            if(conzonDict.get(line.get(i)).getName().contains("IC") && !(i == 0 || i == line.size() - 1) )
                 continue;
-            System.out.print(conzonDict.get(line.get(i)) + (i != 0 ? " - " : "\n"));
+            System.out.print(conzonDict.get(line.get(i)).getName() + (i != 0 ? " - " : "\n"));
         }
     }
 
@@ -153,6 +164,7 @@ public class Dfs
         rst += 1800;
         return (int) rst;
     }
+
 }
 
 class Node implements Comparable<Node>
@@ -170,5 +182,23 @@ class Node implements Comparable<Node>
     public int compareTo(Node o)
     {
         return Integer.compare(this.cost, o.cost);
+    }
+}
+class AStarNode implements Comparable<AStarNode>
+{
+    int idx;
+    int cost, h;
+
+    public AStarNode(int idx, int cost, int h)
+    {
+        this.idx = idx;
+        this.cost = cost;
+        this.h = h;
+    }
+
+    @Override
+    public int compareTo(AStarNode o)
+    {
+        return Integer.compare(this.cost + this.h, o.cost+ o.h);
     }
 }
