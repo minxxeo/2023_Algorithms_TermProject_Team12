8e41a2df5b5792ba74c0c17e9f37ceaca2a97d3b
One-HyeWon <5876675@naver.com>
2023-12-04 21:53:11 +0900

    [feat]: 드롭다운 추가

--- ui/src/components/TaxiFinder.js
+++ ui/src/components/TaxiFinder.js
@@ -1,15 +1,48 @@
-// TaxiFinder.js
-
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Map from "./Map";
 import { ConzoneCoordinates } from "./ConzoneCoordinates";
 import "../App.css";
 
 const TaxiFinder = () => {
-  const [startLocation, setStartLocation] = useState(null);
-  const [endLocation, setEndLocation] = useState(null);
+  const [startLocation, setStartLocation] = useState("");
+  const [endLocation, setEndLocation] = useState("");
   const [result, setResult] = useState(null);
   const [loading, setLoading] = useState(false);
+  const [conzoneNames, setConzoneNames] = useState([]);
+  const [filteredConzoneNames, setFilteredConzoneNames] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+
+  useEffect(() => {
+    const fetchConzoneNames = async () => {
+      try {
+        const response = await fetch("/assets/conzonloc.txt");
+        const textData = await response.text();
+        const conzoneNamesArray = parseConzoneNames(textData);
+        setConzoneNames(conzoneNamesArray);
+        setFilteredConzoneNames(conzoneNamesArray);
+      } catch (error) {
+        console.error("Error fetching conzone names:", error);
+      }
+    };
+
+    fetchConzoneNames();
+  }, []);
+
+  const parseConzoneNames = (textData) => {
+    const lines = textData.split("\n");
+    const conzoneNamesArray = lines
+      .slice(1)
+      .map((line, index) => {
+        const values = line.split(",");
+        return values[15]?.replace(/"/g, "");
+      })
+      .filter((name) => name);
+
+    const uniqueConzoneNamesArray = Array.from(new Set(conzoneNamesArray));
+
+    // 가나다순으로 정렬
+    return uniqueConzoneNamesArray.sort((a, b) => a.localeCompare(b));
+  };
 
   const handleMapClick = ({ latlng }) => {
     if (latlng) {
@@ -25,9 +58,10 @@ const TaxiFinder = () => {
   const handleFindTaxi = () => {
     try {
       setResult({
-        minDistance: 364.26,
-        fee: "339,390 원",
-        path: "부산TG - 노포IC - 노포JC - 양산JC - 양산IC - 통도사IC - 서울주JC - 서울산IC - 언양JC - 활천IC - 경주IC - 건천IC - 영천JC - 북안IC - 동영천IC - 화산JC - 신녕IC - 동군위IC - 군위JC - 서군위IC - 도개IC - 상주JC - 낙동JC - 남상주IC - 화서IC - 속리산IC - 보은IC - 회인IC - 문의청남대IC - 청주JC - 남이JC - 청주IC - 옥산IC - 독립기념관IC - 천안JC - 천안IC - 북천안IC - 안성IC - 안성JC - 남사진위IC - 오산IC - 동탄JC - 기흥동탄IC - 기흥IC - 수원신갈IC",
+        minDistance: 361.26,
+        fee: "482,100 원",
+        time: "3시간 48분",
+        path: "부산TG - 노포IC - 노포JC - 양산JC - 양산IC - 통도사IC - 서울주JC - 서울산IC - 언양JC - 활천IC - 경주IC - 건천IC - 영천JC - 북안IC - 동영천IC - 화산JC - 신녕IC - 동군위IC - 군위JC - 서군위IC - 도개IC - 상주JC - 낙동JC - 남상주IC - 화서IC - 속리산IC - 보은IC - 회인IC - 문의청남대IC - 청주JC - 남이JC - 청주IC - 옥산IC - 독립기념관IC - 천안JC - 천안IC - 북천안IC - 안성IC - 안성JC - 남사진위IC - 오산IC - 동탄JC - 기흥동탄IC - 기흥IC - 수원신갈IC", // (생략) 실제 로직에 맞게 수정 필요
       });
       setLoading(false);
 
@@ -35,10 +69,8 @@ const TaxiFinder = () => {
       Object.values(ConzoneCoordinates).forEach(({ lat, lng }) => {
         bounds.extend(new window.kakao.maps.LatLng(lat, lng));
       });
-
       window.map.setBounds(bounds);
 
-      // 모달 표시
       document.getElementById("resultModal").classList.add("show");
     } catch (error) {
       console.error("택시 찾기 오류:", error);
@@ -47,8 +79,23 @@ const TaxiFinder = () => {
   };
 
   const handleCloseModal = () => {
-    // 모달 닫기
     document.getElementById("resultModal").classList.remove("show");
+  };
+
+  const handleInputChange = (event, setLocation, setFilteredNames) => {
+    const value = event.target.value;
+    setInputValue(value);
+
+    // 입력 값이 포함된 이름만 필터링
+    const filteredNames = conzoneNames.filter((name) => name.includes(value));
+
+    setFilteredNames(filteredNames);
+    setLocation(value);
+  };
+
+  const handleDropdownSelect = (selectedName, setLocation) => {
+    setLocation(selectedName);
+    setInputValue(selectedName);
   };
 
   return (
@@ -60,9 +107,51 @@ const TaxiFinder = () => {
       />
       <div className="taxi-finder-form">
         <label>출발지:</label>
-        <input type="text" placeholder="출발지 입력" id="startLocation" />
+        <input
+          type="text"
+          placeholder="출발지 입력"
+          id="startLocation"
+          value={startLocation}
+          onChange={(e) =>
+            handleInputChange(e, setStartLocation, setFilteredConzoneNames)
+          }
+        />
+        <div className="dropdown">
+          <select
+            onChange={(e) =>
+              handleDropdownSelect(e.target.value, setStartLocation)
+            }
+          >
+            {filteredConzoneNames.map((name, index) => (
+              <option key={index} value={name}>
+                {name}
+              </option>
+            ))}
+          </select>
+        </div>
         <label>도착지:</label>
-        <input type="text" placeholder="도착지 입력" id="endLocation" />
+        <input
+          type="text"
+          placeholder="도착지 입력"
+          id="endLocation"
+          value={endLocation}
+          onChange={(e) =>
+            handleInputChange(e, setEndLocation, setFilteredConzoneNames)
+          }
+        />
+        <div className="dropdown">
+          <select
+            onChange={(e) =>
+              handleDropdownSelect(e.target.value, setEndLocation)
+            }
+          >
+            {filteredConzoneNames.map((name, index) => (
+              <option key={index} value={name}>
+                {name}
+              </option>
+            ))}
+          </select>
+        </div>
         <button onClick={handleFindTaxi}>찾기</button>
         {loading && <p style={{ marginTop: "10px" }}>로딩 중...</p>}
       </div>
@@ -84,6 +173,7 @@ const TaxiFinder = () => {
           </button>
           <p style={{ margin: "5px 0" }}>이동 거리: {result.minDistance} km</p>
           <p style={{ margin: "5px 0" }}>이동 요금: {result.fee}</p>
+          <p style={{ margin: "5px 0" }}>이동 시간: {result.time}</p>
           <p style={{ margin: "5px 0" }}>이동 경로: {result.path}</p>
         </div>
       )}
